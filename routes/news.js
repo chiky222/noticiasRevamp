b3f9977c1bb07e00a917a1580a8869e01d4cd350
chiky222 <76072829+chiky222@users.noreply.github.com>
2023-08-15 13:25:51 -0300

    modify get

--- routes/news.js
+++ routes/news.js
@@ -10,7 +10,7 @@ router.use(express.urlencoded({ extended: true }));
 
 router.get("/", async (req, res) => {
   const model = await Noticia.find({})
-    .then((data) => console.log(data))
+    .then((data) => res.send(data))
     .catch((err) => console.log(err));
 });
 
